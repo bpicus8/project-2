75be676ffa727a99932b58b60639d070cec1bdad
BMerchant <112279397+BMerchant123@users.noreply.github.com>
2023-04-06 14:11:16 -0500

    fixing the js

--- js/script.js
+++ js/script.js
@@ -2,5 +2,5 @@ function submit(){
   var name = document.forms["contactUs"]["name"].value
   var email = document.forms["contactUs"]["email"].value
   document.getElementById('notification').play();
-  alert("Thank you for contacting us " + TOS + ". We will email you at " + email " when we are available.")
+  alert("Thank you for contacting us " + name + ". We will email you at " + email " when we are available.")
 }
