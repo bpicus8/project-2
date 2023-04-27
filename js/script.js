9a076be2ea4101d7d16900902eb83ae47872095f
BMerchant <112279397+BMerchant123@users.noreply.github.com>
2023-04-27 11:37:16 -0500

    Updating Brett's changes that he made to the js manually due to unknown pull request conflicts

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
-function submit(){
-  var name = document.forms["contactUs"]["name"].value
-  var email = document.forms["contactUs"]["email"].value
-  document.getElementById('notification').play();
-  alert("Thank you for contacting us " + name + ". We will email you at " + email + " when we are available.")
+function submitForm(){
+var name = document.forms["contactUs"]["name"].value;
+var email = document.forms["contactUs"]["email"].value;
+document.getElementById('notification').play();
+alert("Thank you for contacting us " + name + ". We will email you at " + email + " when we are available.");
 }
+
+const sendButton = document.getElementById("send");
+
+sendButton.addEventListener("click", function(event) {
+event.preventDefault(); // Prevents the form from being submitted
+alert("Thank you for your message!");
+});
