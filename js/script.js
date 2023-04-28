b51d2281875796525b3421aa9810cdbb9f51bbe3
bpicus08 <bpicus@hawk.iit.edu>
2023-04-27 22:32:58 -0500

    edited cascade

--- js/script.js
+++ js/script.js
@@ -1,8 +1,4 @@
 function submitForm(){
-var name = document.forms["contactUs"]["name"].value;
-var email = document.forms["contactUs"]["email"].value;
-document.getElementById('notification').play();
-alert("Thank you for contacting us " + name + ". We will email you at " + email + " when we are available.");
   var name = document.forms["contactUs"]["name"].value;
   var email = document.forms["contactUs"]["email"].value;
   document.getElementById('notification').play();
@@ -12,9 +8,6 @@ alert("Thank you for contacting us " + name + ". We will email you at " + email 
 const sendButton = document.getElementById("send");
 
 sendButton.addEventListener("click", function(event) {
-event.preventDefault(); // Prevents the form from being submitted
-alert("Thank you for your message!");
-});
  event.preventDefault(); // Prevents the form from being submitted
  alert("Thank you for your message!");
 });
